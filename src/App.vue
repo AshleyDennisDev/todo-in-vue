c4fa9450e51107a8f36a5155c8dae6ac2777c0cc
Ashley Dennis <adenn1852@gmail.com>
2022-06-03 10:39:26 -0400

    first push

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import TaskListVue from './TaskList.vue';
+
+export default{
+  components: {
+    TaskListVue
+  }
+}
+</script>
+
+<template>
+<TaskListVue />
+</template>
+
+
+<style>
+#app {
+  background: rgb(190, 206, 174);
+  margin: 4rem 8rem 4rem 8rem;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 5rem rgba(0, 0, 0, 0.1);
+  max-width: 60rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/TaskList.vue
@@ -0,0 +1,90 @@
+<script>
+let id = 0
+
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo, done: false, editing: false })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+    }
+  }
+
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input class="form" v-model="newTodo">
+    <button class="add-btn">Add Todo</button>    
+  </form>
+  <ul class="list">
+    <li v-for="todo in todos" :key="todo.id">
+    <input class="checkbox" type="checkbox" v-model="todo.done" />
+    <span v-if="!todo.editing" :class="{done: todo.done}">{{ todo.text }}</span>
+    <input class="form-2" v-if="todo.editing" style="display: inline" type="text" v-model="todo.text">
+    <button class="edit-btn" v-if="!todo.editing" @click="todo.editing = true">Edit</button>
+    <button class="update-btn" v-if="todo.editing" @click="todo.editing = false"> Update </button>
+    <button class="delete-btn" @click="removeTodo(todo)">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<style>
+.form{
+    font-size: 2rem;
+    border: 4px solid rgb(152, 151, 151);
+    margin-top: 5rem;
+    margin-right: 2rem;
+    display: inline;
+    justify-content: center;
+
+}
+.form-2{
+    font-size: 2rem;
+    border: 4px solid rgb(152, 151, 151);
+    margin-bottom: 1rem;
+    margin-right: 2rem
+}
+.add-btn{
+    font-size: 2rem;
+    background-color: bisque;
+}
+.edit-btn{
+    font-size: 2rem;
+    margin-right: 1rem;
+    margin-left: 1rem;
+    color: bisque;
+    background-color: rgb(61, 58, 58);
+}
+.update-btn{
+    font-size: 2rem;
+    margin-right: 1rem;
+    margin-left: 1rem;
+    color: rgb(218, 129, 20);
+    background-color: rgb(61, 58, 58);
+}
+.delete-btn{
+    font-size: 2rem;
+    margin-right: 1rem;
+    margin-left: 1rem;
+    color:rgb(221, 72, 72);
+    background-color: rgb(61, 58, 58);
+}
+.list{
+    list-style: none;
+    font-size: 2rem;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+.checkbox{
+   border-radius: 4rem;
+}
+</style>
